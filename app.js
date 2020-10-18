6f9be550bc6034ccea87ce70367c75f49247953d
Vadim Troshkov <tvaa@ya.ru>
2020-10-18 22:27:50 +0300

    18102020-2

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.post('/signin', celebrate({
     password: Joi.string().required().min(6).max(30),
   }),
 }), login);
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
