b5b04015da02c529f75b623d31a5fc22682460c4
Vadim Troshkov <tvaa@ya.ru>
2020-10-21 00:06:10 +0300

    20102020-1

--- app.js
+++ app.js
@@ -68,14 +68,14 @@ app.get('/crash-test', () => {
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(6).max(30),
+    password: Joi.string().required().max(30),
   }),
 }), login);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(6).max(30),
+    password: Joi.string().required().max(30),
   }),
 }), createUser);
 
